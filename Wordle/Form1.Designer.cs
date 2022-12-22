56c2b4061387a47acad6d426770982d59c26f355
nortrix0 <minecrafternortrix@gmail.com>
2022-12-22 15:57:29 -0600

    Add project files.

---
+++ Wordle/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Wordle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.clm1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clm2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clm3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clm4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clm5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clm1,
+            this.clm2,
+            this.clm3,
+            this.clm4,
+            this.clm5});
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.RowTemplate.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.dataGridView1.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.SystemColors.Window;
+            this.dataGridView1.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(253, 135);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // clm1
+            // 
+            this.clm1.HeaderText = "";
+            this.clm1.Name = "clm1";
+            this.clm1.ReadOnly = true;
+            this.clm1.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.clm1.Width = 50;
+            // 
+            // clm2
+            // 
+            this.clm2.HeaderText = "";
+            this.clm2.Name = "clm2";
+            this.clm2.ReadOnly = true;
+            this.clm2.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.clm2.Width = 50;
+            // 
+            // clm3
+            // 
+            this.clm3.HeaderText = "";
+            this.clm3.Name = "clm3";
+            this.clm3.ReadOnly = true;
+            this.clm3.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.clm3.Width = 50;
+            // 
+            // clm4
+            // 
+            this.clm4.HeaderText = "";
+            this.clm4.Name = "clm4";
+            this.clm4.ReadOnly = true;
+            this.clm4.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.clm4.Width = 50;
+            // 
+            // clm5
+            // 
+            this.clm5.HeaderText = "";
+            this.clm5.Name = "clm5";
+            this.clm5.ReadOnly = true;
+            this.clm5.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.clm5.Width = 50;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(13, 154);
+            this.maskedTextBox1.Mask = "AAAAA";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(84, 20);
+            this.maskedTextBox1.TabIndex = 0;
+            this.maskedTextBox1.ValidatingType = typeof(int);
+            this.maskedTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.maskedTextBox1_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 182);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clm1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clm2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clm3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clm4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clm5;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+    }
+}
+
---
+++ Wordle/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Wordle
+{
+    public partial class Form1 : Form
+    {
+        private string word = "";
+        private static string[] words = Properties.Resources.words.Replace("\r","").ToUpper().Split('\n');
+        private string currWord = "";
+        private Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            GetNewWord();
+        }
+        private void GetNewWord() => word = words[rand.Next(words.Length - 1)];
+        private void CheckWord()
+        {
+            currWord = maskedTextBox1.Text.ToUpper();
+            var entry = dataGridView1.Rows.Add(currWord[0], currWord[1], currWord[2], currWord[3], currWord[4]);
+            for (int i = 0; i < 5; i++)
+            {
+                if (word[i] == currWord[i])
+                    dataGridView1.Rows[entry].Cells[i].Style.BackColor = Color.Green;
+                else if (!word.Contains(currWord[i]))
+                    dataGridView1.Rows[entry].Cells[i].Style.BackColor = Color.Gray;
+                else
+                {
+                    int count = word.Count(t => t == currWord[i]);
+                    for (int j = 0; j < 5; j++)
+                    {
+                        dataGridView1.Rows[entry].Cells[i].Style.BackColor = Color.Yellow;
+                        if (dataGridView1.Rows[entry].Cells[j].Style.BackColor == Color.Green && dataGridView1.Rows[entry].Cells[j].Value.ToString() == currWord[i].ToString())
+                        {
+                            count--;
+                        }
+                    }
+                    if (count <= 0)
+                        dataGridView1.Rows[entry].Cells[i].Style.BackColor = Color.Gray;
+                }
+            }
+            if (currWord == word)
+            {
+                MessageBox.Show("CORRECT!");
+                dataGridView1.Rows.Clear();
+            }
+            else if (entry == 5)
+            {
+                MessageBox.Show($"FAILED!\nWord: {word}");
+                GetNewWord();
+                dataGridView1.Rows.Clear();
+            }
+            maskedTextBox1.Text = "";
+        }
+        private void maskedTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                CheckWord();
+        }
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e) => dataGridView1.ClearSelection();
+    }
+}
